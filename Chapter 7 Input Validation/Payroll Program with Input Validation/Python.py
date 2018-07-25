65803abee0be51610662aa0c53ede780b3368b97
Kynan <36722419+kynants@users.noreply.github.com>
2018-07-24 23:21:52 -0400

    Update Python.py

--- Chapter 7 Input Validation/Payroll Program with Input Validation/Python.py
+++ Chapter 7 Input Validation/Payroll Program with Input Validation/Python.py
@@ -1,13 +1,22 @@
+# IMPORT
+# https://docs.python.org/3/library/time.html
+import time
+
 # PAY RATE PROMPT
-payRate = float(input("Enter your hourly pay rate: "))
+pay_rate = float(input("Enter your hourly pay rate: "))
 
 # PAY RATE VALIDATION
-while payRate < 7.5 and payRate > 18.25:
-    payRate = float(print("Error. Please enter a valid wage: "))
+while pay_rate < 7.5 or pay_rate > 18.25:
+    pay_rate = float(input("Error. Please enter a valid wage: "))
 
 # HOURS WORKED PROMPT
-hrsWork = int(input("How many hours did you work? "))
+hrs_worked = int(input("How many hours did you work? "))
 
 # HOURS WORKED VALIDATION
-while hrsWork < 0 and hrsWork > 40:
-    
+while hrs_worked < 0 or hrs_worked > 40:
+    print("Error. Number of hours worked must be from 0 to 40.")
+    time.sleep(2)
+    hrs_worked = float(input("Please enter a valid number of hours worked: "))
+
+# THE PROGRAM FINALLY DISPLAYS THE DAY'S WAGE
+print("Your weekly salary is: ${}".format(pay_rate * hrs_worked))
